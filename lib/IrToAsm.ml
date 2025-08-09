2451ffe48bb94ade4b24697a692ebd6b9f79e93e
辛屹 <163866348+xinkyu@users.noreply.github.com>
2025-08-09 20:50:10 +0800

    3.3.5

--- lib/IrToAsm.ml
+++ lib/IrToAsm.ml
@@ -1,7 +1,9 @@
 open Ir
+open Reg_alloc
 
 let stack_offset = ref 0
 let v_env = Hashtbl.create 1600
+let reg_var_map = ref StringMap.empty  (* 变量到寄存器的映射 *)
 
 let get_sto var =
   try Hashtbl.find v_env var
@@ -15,9 +17,11 @@ let all_st var =
     Hashtbl.add v_env var !stack_offset;
     !stack_offset
 
-let operand_str = function
-  | Reg r | Var r -> Printf.sprintf "%d(sp)" (get_sto r)
-  | Imm i -> Printf.sprintf "%d" i
+(* 获取变量的寄存器，如果已分配 *)
+let get_var_reg var =
+  match get_var_register !reg_var_map var with
+  | Some reg -> Some reg
+  | None -> None
 
 (* 检查是否是2的幂 *)
 let is_power_of_2 n = n > 0 && (n land (n - 1)) = 0
@@ -30,30 +34,44 @@ let log2 n =
   in
   aux 0 n
 
-let l_operand (reg : string) (op : operand) : string =
+(* 优化的操作数加载 - 优先使用寄存器 *)
+let l_operand (dest_reg : string) (op : operand) : string =
   match op with
-  | Imm i -> Printf.sprintf "\tli %s, %d\n" reg i
-  | Reg r | Var r -> Printf.sprintf "\tlw %s, %d(sp)\n" reg (get_sto r)
+  | Imm i -> Printf.sprintf "\tli %s, %d\n" dest_reg i
+  | Reg r | Var r -> 
+      match get_var_reg r with
+      | Some src_reg -> 
+          if src_reg = dest_reg then ""  (* 相同寄存器不需要移动 *)
+          else Printf.sprintf "\tmv %s, %s\n" dest_reg src_reg
+      | None -> Printf.sprintf "\tlw %s, %d(sp)\n" dest_reg (get_sto r)
+
+(* 优化的操作数存储 - 如果有寄存器，存储到寄存器 *)
+let store_result (dst : operand) (src_reg : string) : string =
+  match dst with
+  | Reg r | Var r ->
+      let dst_off = all_st r in
+      match get_var_reg r with
+      | Some dst_reg -> 
+          if dst_reg = src_reg then ""  (* 相同寄存器不需要移动 *)
+          else Printf.sprintf "\tmv %s, %s\n" dst_reg src_reg
+      | None -> Printf.sprintf "\tsw %s, %d(sp)\n" src_reg dst_off
+  | _ -> failwith "Invalid destination"
 
 let com_inst (inst : ir_inst) : string =
   match inst with
   | Binop (op, dst, lhs, rhs) ->
-      let dst_off =
-        all_st
-          (match dst with Reg r | Var r -> r | _ -> failwith "Bad dst")
-      in
       let lhs_code = l_operand "t1" lhs in
       
-      (* 根据操作符和右操作数生成代码 *)
+      (* 优化乘除法 *)
       let (rhs_code, op_code) = 
         match op, rhs with
         | "*", Imm n when is_power_of_2 n ->
-            (* 优化：乘以2的幂用移位代替 *)
+            (* 乘法优化为移位 *)
             let shift = log2 n in
             Printf.sprintf "\tli t2, %d\n" shift,
             "\tsll t0, t1, t2\n"
         | "/", Imm n when is_power_of_2 n ->
-            (* 优化：除以2的幂用移位代替 *)
+            (* 除法优化为移位 *)
             let shift = log2 n in
             Printf.sprintf "\tli t2, %d\n" shift,
             "\tsra t0, t1, t2\n"
@@ -73,13 +91,9 @@ let com_inst (inst : ir_inst) : string =
         | _, _ -> l_operand "t2" rhs, failwith ("Unknown binop: " ^ op)
       in
       
-      lhs_code ^ rhs_code ^ op_code ^ Printf.sprintf "\tsw t0, %d(sp)\n" dst_off
+      lhs_code ^ rhs_code ^ op_code ^ store_result dst "t0"
       
   | Unop (op, dst, src) ->
-      let dst_off =
-        all_st
-          (match dst with Reg r | Var r -> r | _ -> failwith "Bad dst")
-      in
       let load_src = l_operand "t1" src in
       let op_code =
         match op with
@@ -88,23 +102,45 @@ let com_inst (inst : ir_inst) : string =
         | "+" -> "\tmv t0, t1\n"
         | _ -> failwith ("Unknown unop: " ^ op)
       in
-      load_src ^ op_code ^ Printf.sprintf "\tsw t0, %d(sp)\n" dst_off
+      load_src ^ op_code ^ store_result dst "t0"
       
   | Assign (dst, src) ->
-      let dst_off =
-        all_st
-          (match dst with Reg r | Var r -> r | _ -> failwith "Bad dst")
-      in
-      let load_src = l_operand "t0" src in
-      load_src ^ Printf.sprintf "\tsw t0, %d(sp)\n" dst_off
+      (* 直接从源到目标的优化赋值 *)
+      match dst, src with
+      | (Reg dst_name | Var dst_name), (Reg src_name | Var src_name) ->
+          (* 两者都是变量 *)
+          (match get_var_reg dst_name, get_var_reg src_name with
+           | Some dst_reg, Some src_reg ->
+               if dst_reg = src_reg then ""
+               else Printf.sprintf "\tmv %s, %s\n" dst_reg src_reg
+           | Some dst_reg, None ->
+               Printf.sprintf "\tlw %s, %d(sp)\n" dst_reg (get_sto src_name)
+           | None, Some src_reg ->
+               Printf.sprintf "\tsw %s, %d(sp)\n" src_reg (all_st dst_name)
+           | None, None ->
+               Printf.sprintf "\tlw t0, %d(sp)\n\tsw t0, %d(sp)\n" 
+                 (get_sto src_name) (all_st dst_name))
+      | (Reg dst_name | Var dst_name), Imm i ->
+          (* 源是立即数 *)
+          (match get_var_reg dst_name with
+           | Some dst_reg -> Printf.sprintf "\tli %s, %d\n" dst_reg i
+           | None -> Printf.sprintf "\tli t0, %d\n\tsw t0, %d(sp)\n" i (all_st dst_name))
+      | _, _ ->
+          (* 其他情况 *)
+          let load_src = l_operand "t0" src in
+          load_src ^ store_result dst "t0"
       
   | Load (dst, src) ->
-      let dst_off =
-        all_st
-          (match dst with Reg r | Var r -> r | _ -> failwith "Bad dst")
-      in
       let src_code = l_operand "t1" src in
-      src_code ^ "\tlw t0, 0(t1)\n" ^ Printf.sprintf "\tsw t0, %d(sp)\n" dst_off
+      let dst_code = 
+        match dst with
+        | Reg r | Var r ->
+            (match get_var_reg r with
+             | Some dst_reg -> Printf.sprintf "\tlw %s, 0(t1)\n" dst_reg
+             | None -> Printf.sprintf "\tlw t0, 0(t1)\n\tsw t0, %d(sp)\n" (all_st r))
+        | _ -> failwith "Invalid destination"
+      in
+      src_code ^ dst_code
       
   | Store (dst, src) ->
       let dst_code = l_operand "t1" dst in
@@ -112,10 +148,18 @@ let com_inst (inst : ir_inst) : string =
       dst_code ^ src_code ^ "\tsw t2, 0(t1)\n"
       
   | Call (dst, fname, args) ->
-      let dst_off =
-        all_st
-          (match dst with Reg r | Var r -> r | _ -> failwith "Bad dst")
+      (* 调用函数时，需要保存和恢复寄存器 *)
+      
+      (* 保存寄存器到栈上 *)
+      let saved_regs = StringMap.fold (fun var info acc ->
+          match info.reg with
+          | Some reg -> 
+              Printf.sprintf "\tsw %s, %d(sp)\n" reg (all_st var) ^ acc
+          | None -> acc
+        ) !reg_var_map ""
       in
+      
+      (* 准备参数 *)
       let args_code =
         List.mapi
           (fun i arg ->
@@ -126,7 +170,23 @@ let com_inst (inst : ir_inst) : string =
           args
         |> String.concat ""
       in
-      args_code ^ Printf.sprintf "\tcall %s\n\tsw a0, %d(sp)\n" fname dst_off
+      
+      (* 函数调用 *)
+      let call_code = Printf.sprintf "\tcall %s\n" fname in
+      
+      (* 恢复寄存器 *)
+      let restored_regs = StringMap.fold (fun var info acc ->
+          match info.reg with
+          | Some reg -> 
+              acc ^ Printf.sprintf "\tlw %s, %d(sp)\n" reg (get_sto var)
+          | None -> acc
+        ) !reg_var_map ""
+      in
+      
+      (* 保存返回值 *)
+      let result_code = store_result dst "a0" in
+      
+      saved_regs ^ args_code ^ call_code ^ restored_regs ^ result_code
       
   | Ret None ->
       let ra_offset = get_sto "ra" in
@@ -150,64 +210,105 @@ let com_inst (inst : ir_inst) : string =
       
   | Label label -> Printf.sprintf "%s:\n" label
 
-(* 优化尾递归调用 *)
-let optimize_tail_calls (func_name : string) (args : string list) (insts : ir_inst list) : ir_inst list =
+(* 正确实现尾递归优化 *)
+let optimize_tail_recursion (f : ir_func) : ir_func =
+  (* 检查是否有自身递归调用 *)
+  let has_self_call = List.exists (function
+      | Call (_, name, _) when name = f.name -> true
+      | _ -> false
+    ) f.body
+  in
+  
+  if not has_self_call then f else
+  
+  (* 将参数移动指令添加到函数开头 *)
+  let param_moves = 
+    List.mapi (fun i name ->
+        (* a0-a7是参数寄存器 *)
+        if i < 8 then
+          match get_var_reg name with
+          | Some reg -> [Assign (Var name, Reg ("a" ^ string_of_int i))]
+          | None -> [Assign (Var name, Reg ("a" ^ string_of_int i))]
+        else
+          let stack_offset = -4 * (i - 8) in
+          [Load (Var name, Imm stack_offset)]
+      ) f.args
+    |> List.flatten
+  in
+  
+  (* 处理尾递归调用 *)
   let rec process_insts acc = function
     | [] -> List.rev acc
-    | Call (dst, fname, call_args) :: Ret (Some ret_val) :: rest 
-      when fname = func_name && 
+    | Call (dst, fname, args) :: Ret (Some ret_val) :: rest 
+      when fname = f.name && 
            (match dst, ret_val with
             | Reg d, Reg r | Var d, Var r | Reg d, Var r | Var d, Reg r -> d = r
             | _ -> false) ->
-        (* 找到尾递归调用，转换为参数赋值和跳转 *)
-        let param_assigns = 
+        (* 找到尾递归调用 *)
+        let setup_args = 
           List.mapi (fun i arg ->
-              if i < List.length args then
-                let param_name = List.nth args i in
-                Assign (Var param_name, arg)
+              if i < 8 then
+                (* 直接移动到参数寄存器 *)
+                match arg with
+                | Imm n -> Assign (Reg ("a" ^ string_of_int i), Imm n)
+                | _ -> Assign (Reg ("a" ^ string_of_int i), arg)
               else
-                Call (dst, fname, call_args) (* 保持原样，不应该发生 *)
-            ) call_args
+                (* 栈上参数 *)
+                let stack_offset = -1600 - 4 * (i - 8) in
+                match arg with
+                | Imm n -> Store (Imm stack_offset, Imm n)
+                | _ -> Assign (Var ("_temp"), arg) :: [Store (Imm stack_offset, Var "_temp")]
+            ) args
+          |> List.flatten
         in
-        process_insts (List.rev (Goto "func_start" :: param_assigns) @ acc) rest
+        
+        (* 跳转到函数开头 *)
+        process_insts (List.rev (Goto "func_start" :: setup_args) @ acc) rest
+        
     | inst :: rest ->
         process_insts (inst :: acc) rest
   in
   
-  (* 只有当函数内有尾递归调用时才添加标签 *)
-  let has_tail_call = 
-    List.exists (function
-      | Call (_, fname, _) :: Ret _ :: _ when fname = func_name -> true
-      | _ -> false
-    ) (List.mapi (fun i inst -> 
-         if i < List.length insts - 1 then [inst; List.nth insts (i+1)]
-         else [inst]
-       ) insts)
+  (* 添加函数入口标签和参数移动指令，然后处理尾递归 *)
+  let optimized_body = 
+    Label "func_start" :: param_moves @ process_insts [] f.body 
   in
   
-  if has_tail_call then
-    Label "func_start" :: process_insts [] insts
-  else
-    insts
+  { f with body = optimized_body }
 
 let com_block (blk : ir_block) : string =
+  (* 分配寄存器 *)
+  reg_var_map := allocate_block_regs blk;
+  
   blk.insts |> List.map com_inst |> String.concat ""
 
 let com_func (f : ir_func) : string =
   Hashtbl.clear v_env;
   stack_offset := 0;
+  
+  (* 分配寄存器 *)
+  reg_var_map := allocate_func_registers f;
+  
+  (* 尾递归优化 *)
+  let f = optimize_tail_recursion f in
 
   (* 参数入栈 *)
   let pae_set =
     List.mapi
       (fun i name ->
         let off = all_st name in
-        if i < 8 then Printf.sprintf "\tsw a%d, %d(sp)\n" i off
-        else
-          Printf.sprintf "\tlw t0, %d(sp)\n\tsw t0, %d(sp)\n"
-            (* offset 为 call 语句将第 i 个参数压入的偏移 *)
-            (-4 * (i - 8))
-            off)
+        match get_var_reg name with
+        | Some reg ->
+            if i < 8 then Printf.sprintf "\tmv %s, a%d\n" reg i
+            else Printf.sprintf "\tlw %s, %d(sp)\n" reg (-4 * (i - 8))
+        | None ->
+            if i < 8 then Printf.sprintf "\tsw a%d, %d(sp)\n" i off
+            else
+              Printf.sprintf "\tlw t0, %d(sp)\n\tsw t0, %d(sp)\n"
+                (* offset 为 call 语句将第 i 个参数压入的偏移 *)
+                (-4 * (i - 8))
+                off
+      )
       f.args
     |> String.concat ""
   in
@@ -217,10 +318,7 @@ let com_func (f : ir_func) : string =
     pae_set ^ Printf.sprintf "\tsw ra, %d(sp)\n" (all_st "ra")
   in
 
-  (* 优化尾递归调用 *)
-  let optimized_body = optimize_tail_calls f.name f.args f.body in
-
-  let body_code = optimized_body |> List.map com_inst |> String.concat "" in
+  let body_code = f.body |> List.map com_inst |> String.concat "" in
 
   let body_code =
     if not (String.ends_with ~suffix:"\tret\n" body_code) then
--- lib/reg_alloc.ml
+++ lib/reg_alloc.ml
@@ -1,8 +1,8 @@
-(* reg_alloc.ml - 寄存器分配优化模块 *)
 open Ir
 
-(* 可用的寄存器列表 (s0-s11) *)
+(* 可用的寄存器列表 (t3-t6, s0-s11) *)
 let available_registers = [
+  "t3"; "t4"; "t5"; "t6";
   "s0"; "s1"; "s2"; "s3"; "s4"; "s5"; 
   "s6"; "s7"; "s8"; "s9"; "s10"; "s11"
 ]
@@ -12,99 +12,88 @@ type var_info = {
   mutable use_count: int;       (* 使用次数 *)
   mutable last_use: int;        (* 最后一次使用的位置 *)
   mutable reg: string option;   (* 分配的寄存器 *)
-  mutable spill: bool;          (* 是否需要溢出到栈 *)
+  mutable in_stack: bool;       (* 是否也在栈上 *)
 }
 
 module StringMap = Map.Make(String)
 
-(* 分析变量使用情况 *)
-let analyze_var_usage (insts : ir_inst list) : var_info StringMap.t =
+(* 分析基本块中的变量使用情况 *)
+let analyze_block_usage (block : ir_inst list) : var_info StringMap.t =
   let var_map = ref StringMap.empty in
   let inst_index = ref 0 in
   
-  (* 初始化变量信息 *)
-  let get_or_create_info var =
+  (* 获取或创建变量信息 *)
+  let get_var_info var =
     match StringMap.find_opt var !var_map with
     | Some info -> info
-    | None ->
-        let info = { use_count = 0; last_use = 0; reg = None; spill = false } in
+    | None -> 
+        let info = { use_count = 0; last_use = 0; reg = None; in_stack = true } in
         var_map := StringMap.add var info !var_map;
         info
   in
   
-  (* 更新变量使用信息 *)
-  let update_var_use var =
-    let info = get_or_create_info var in
+  (* 记录变量使用 *)
+  let record_use var =
+    let info = get_var_info var in
     info.use_count <- info.use_count + 1;
     info.last_use <- !inst_index
   in
   
-  (* 分析操作数使用 *)
-  let analyze_operand = function
-    | Reg name | Var name -> update_var_use name
+  (* 记录操作数使用 *)
+  let record_operand = function
+    | Reg name | Var name -> record_use name
     | Imm _ -> ()
   in
   
-  (* 遍历所有指令 *)
+  (* 分析指令 *)
   List.iter (fun inst ->
     incr inst_index;
     match inst with
-    | Assign (dst, src) -> 
-        (match dst with 
-         | Reg name | Var name -> update_var_use name 
-         | _ -> ());
-        analyze_operand src
     | Binop (_, dst, src1, src2) ->
-        (match dst with 
-         | Reg name | Var name -> update_var_use name 
-         | _ -> ());
-        analyze_operand src1;
-        analyze_operand src2
+        (match dst with Reg name | Var name -> record_use name | _ -> ());
+        record_operand src1;
+        record_operand src2
     | Unop (_, dst, src) ->
-        (match dst with 
-         | Reg name | Var name -> update_var_use name 
-         | _ -> ());
-        analyze_operand src
+        (match dst with Reg name | Var name -> record_use name | _ -> ());
+        record_operand src
+    | Assign (dst, src) ->
+        (match dst with Reg name | Var name -> record_use name | _ -> ());
+        record_operand src
     | Call (dst, _, args) ->
-        (match dst with 
-         | Reg name | Var name -> update_var_use name 
-         | _ -> ());
-        List.iter analyze_operand args
+        (match dst with Reg name | Var name -> record_use name | _ -> ());
+        List.iter record_operand args
     | Load (dst, src) ->
-        (match dst with 
-         | Reg name | Var name -> update_var_use name 
-         | _ -> ());
-        analyze_operand src
+        (match dst with Reg name | Var name -> record_use name | _ -> ());
+        record_operand src
     | Store (addr, value) ->
-        analyze_operand addr;
-        analyze_operand value
+        record_operand addr;
+        record_operand value
     | IfGoto (cond, _) ->
-        analyze_operand cond
+        record_operand cond
     | Ret (Some op) ->
-        analyze_operand op
+        record_operand op
     | _ -> ()
-  ) insts;
+  ) block;
   
   !var_map
 
 (* 分配寄存器 *)
-let allocate_registers (var_map : var_info StringMap.t) : unit =
-  (* 根据使用频率排序变量 *)
+let allocate_block_registers (var_map : var_info StringMap.t) : unit =
+  (* 按使用频率排序变量 *)
   let vars = 
     StringMap.bindings var_map
-    |> List.map (fun (name, info) -> (name, info))
+    |> List.filter (fun (_, info) -> info.use_count > 1) (* 只为多次使用的变量分配寄存器 *)
     |> List.sort (fun (_, info1) (_, info2) -> 
-        compare info2.use_count info1.use_count)
+        compare info2.use_count info1.use_count) (* 使用频率从高到低 *)
   in
   
-  (* 分配可用寄存器 *)
+  (* 分配寄存器 *)
   let rec allocate reg_list = function
     | [] -> ()
-    | (_, info) :: rest ->  (* 修改：前面添加下划线表示未使用的变量 *)
+    | (var, info) :: rest ->
         match reg_list with
         | [] -> 
-            (* 没有可用寄存器，设置为需要溢出 *)
-            info.spill <- true;
+            (* 没有更多寄存器可用 *)
             allocate [] rest
         | reg :: regs ->
             (* 分配寄存器 *)
@@ -114,28 +103,24 @@ let allocate_registers (var_map : var_info StringMap.t) : unit =
   
   allocate available_registers vars
 
-(* 获取变量的寄存器或栈位置 *)
-let get_var_location (var_map : var_info StringMap.t) (var_name : string) : 
-    [`Reg of string | `Stack] =
-  match StringMap.find_opt var_name var_map with
-  | Some info -> 
-      (match info.reg with
-       | Some reg -> `Reg reg
-       | None -> `Stack)
-  | None -> `Stack
+(* 分析整个函数的变量使用 *)
+let analyze_func_usage (func : ir_func) : var_info StringMap.t =
+  analyze_block_usage func.body
 
-(* 针对基本块的寄存器分配 *)
-let allocate_registers_for_block (blk : ir_block) : var_info StringMap.t =
-  let var_map = analyze_var_usage blk.insts in
-  allocate_registers var_map;
+(* 为整个函数分配寄存器 *)
+let allocate_func_registers (func : ir_func) : var_info StringMap.t =
+  let var_map = analyze_func_usage func in
+  allocate_block_registers var_map;
   var_map
 
-(* 针对函数的寄存器分配 *)
-let allocate_registers_for_func (f : ir_func_o) : var_info StringMap.t =
-  (* 合并所有基本块的指令 *)
-  let all_insts = 
-    List.fold_left (fun acc blk -> acc @ blk.insts) [] f.blocks
-  in
-  let var_map = analyze_var_usage all_insts in
-  allocate_registers var_map;
+(* 为基本块分配寄存器 *)
+let allocate_block_regs (block : ir_block) : var_info StringMap.t =
+  let var_map = analyze_block_usage block.insts in
+  allocate_block_registers var_map;
   var_map
+
+(* 查询变量对应的物理寄存器 *)
+let get_var_register (var_map : var_info StringMap.t) (var : string) : string option =
+  match StringMap.find_opt var var_map with
+  | Some info -> info.reg
+  | None -> None
