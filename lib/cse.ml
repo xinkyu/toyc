c716387c59fc5ce18937cf090b08b34440403e32
辛屹 <163866348+xinkyu@users.noreply.github.com>
2025-08-10 12:29:35 +0800

    5.0.6

--- lib/cse.ml
+++ lib/cse.ml
@@ -17,121 +17,98 @@ let has_side_effects = function
   | Call _ | Load _ | Store _ -> true
   | _ -> false
 
-(* 获取操作数名称 *)
-let get_operand_name = function
+(* 从操作数获取名称 *)
+let get_reg_name = function
   | Reg r | Var r -> r
   | _ -> ""
 
-(* 检查操作数是否是寄存器或变量 *)
-let is_reg_or_var = function
-  | Reg _ | Var _ -> true
+(* 比较两个操作数是否是相同的寄存器/变量 *)
+let same_reg op1 op2 =
+  match (op1, op2) with
+  | (Reg r1 | Var r1), (Reg r2 | Var r2) -> r1 = r2
   | _ -> false
 
-(* 对基本块应用CSE *)
+(* 检查操作数是否是立即数 *)
+let is_imm = function
+  | Imm _ -> true
+  | _ -> false
+
+(* 安全的CSE实现 - 只处理最简单和最安全的情况 *)
 let apply_cse (block : ir_block) : ir_block =
-  (* 表达式到结果变量的映射 *)
-  let expr_map = Hashtbl.create 50 in
-  
-  (* 已定义的寄存器集合 *)
+  (* 收集已经定义的寄存器名称 *)
   let defined_regs = Hashtbl.create 50 in
   
-  (* 添加一个寄存器到已定义集合 *)
-  let add_defined_reg = function
-    | Reg r | Var r -> Hashtbl.replace defined_regs r ()
-    | _ -> ()
-  in
-  
-  (* 检查寄存器是否已定义 *)
-  let is_defined_reg = function
-    | Reg r | Var r -> Hashtbl.mem defined_regs r
-    | _ -> true (* 立即数总是已定义的 *)
-  in
-  
-  (* 首先收集所有在基本块中定义的寄存器 *)
+  (* 为了安全起见，先收集所有被定义的寄存器 *)
   List.iter (fun inst ->
     match inst with
     | Binop (_, dst, _, _) 
     | Unop (_, dst, _)
     | Assign (dst, _)
     | Call (dst, _, _)
-    | Load (dst, _) -> add_defined_reg dst
+    | Load (dst, _) ->
+        (match dst with
+        | Reg r | Var r -> Hashtbl.replace defined_regs r ()
+        | _ -> ())
     | _ -> ()
   ) block.insts;
   
-  (* 处理单条指令 *)
-  let process_inst inst acc =
-    match inst with
-    | Binop (_, dst, lhs, rhs) as binop ->
-        (* 只有当操作数都是已定义的寄存器或立即数时才尝试CSE *)
-        if is_defined_reg lhs && is_defined_reg rhs then
-          let sig_opt = make_signature binop in
-          (match sig_opt with
-          | Some signature ->
-              (match Hashtbl.find_opt expr_map signature with
-              | Some existing_reg when is_defined_reg existing_reg ->
-                  (* 已存在相同表达式且结果寄存器已定义，用Assign替代 *)
-                  let assign = Assign (dst, existing_reg) in
-                  add_defined_reg dst;
-                  Hashtbl.add expr_map signature dst;
-                  assign :: acc
-              | _ ->
-                  (* 新表达式，记录并保留 *)
-                  add_defined_reg dst;
-                  Hashtbl.add expr_map signature dst;
-                  binop :: acc)
-          | None -> 
-              add_defined_reg dst;
-              binop :: acc)
-        else (
-          add_defined_reg dst;
-          binop :: acc
-        )
-    
-    | Unop (_, dst, src) as unop ->
-        (* 只有当操作数是已定义的寄存器或立即数时才尝试CSE *)
-        if is_defined_reg src then
-          let sig_opt = make_signature unop in
-          (match sig_opt with
-          | Some signature ->
-              (match Hashtbl.find_opt expr_map signature with
-              | Some existing_reg when is_defined_reg existing_reg ->
-                  (* 已存在相同表达式且结果寄存器已定义，用Assign替代 *)
-                  let assign = Assign (dst, existing_reg) in
-                  add_defined_reg dst;
-                  Hashtbl.add expr_map signature dst;
-                  assign :: acc
-              | _ ->
-                  (* 新表达式，记录并保留 *)
-                  add_defined_reg dst;
-                  Hashtbl.add expr_map signature dst;
-                  unop :: acc)
-          | None -> 
-              add_defined_reg dst;
-              unop :: acc)
-        else (
-          add_defined_reg dst;
-          unop :: acc
-        )
-    
-    | Assign (dst, _) ->
-        (* 对于赋值，我们只需要追踪目标寄存器 *)
-        add_defined_reg dst;
-        inst :: acc
-    
-    | inst when has_side_effects inst ->
-        (* 有副作用的指令会使表达式失效 *)
-        Hashtbl.clear expr_map;
-        (match inst with
-        | Call (dst, _, _) 
-        | Load (dst, _) -> add_defined_reg dst
-        | _ -> ());
-        inst :: acc
-    
-    | _ -> inst :: acc
+  (* 检查寄存器是否已定义 *)
+  let is_defined = function
+    | Reg r | Var r -> Hashtbl.mem defined_regs r
+    | Imm _ -> true
   in
   
-  (* 处理基本块中的所有指令 *)
-  let new_insts = List.fold_right process_inst block.insts [] in
+  (* 收集基本表达式 - 只考虑简单的二元运算和一元运算 *)
+  let expr_map = Hashtbl.create 50 in
+  
+  (* 第一遍：收集所有表达式 *)
+  List.iter (fun inst ->
+    match inst with
+    | Binop (op, dst, lhs, rhs) when is_defined lhs && is_defined rhs ->
+        let sig_opt = make_signature inst in
+        (match sig_opt with
+        | Some signature -> Hashtbl.replace expr_map signature dst
+        | None -> ())
+    | Unop (op, dst, src) when is_defined src ->
+        let sig_opt = make_signature inst in
+        (match sig_opt with
+        | Some signature -> Hashtbl.replace expr_map signature dst
+        | None -> ())
+    | _ -> ()
+  ) block.insts;
+  
+  (* 第二遍：应用优化，但只对安全的情况 *)
+  let process_inst inst =
+    match inst with
+    | Binop (op, dst, lhs, rhs) as binop when is_defined lhs && is_defined rhs ->
+        let sig_opt = make_signature binop in
+        (match sig_opt with
+        | Some signature ->
+            (match Hashtbl.find_opt expr_map signature with
+            | Some existing_reg when is_defined existing_reg && not (same_reg existing_reg dst) ->
+                (* 已存在相同表达式，用Assign替代，但只有当existing_reg和dst不同时 *)
+                Assign (dst, existing_reg)
+            | _ -> inst)
+        | None -> inst)
+    | Unop (op, dst, src) as unop when is_defined src ->
+        let sig_opt = make_signature unop in
+        (match sig_opt with
+        | Some signature ->
+            (match Hashtbl.find_opt expr_map signature with
+            | Some existing_reg when is_defined existing_reg && not (same_reg existing_reg dst) ->
+                (* 已存在相同表达式，用Assign替代，但只有当existing_reg和dst不同时 *)
+                Assign (dst, existing_reg)
+            | _ -> inst)
+        | None -> inst)
+    | inst when has_side_effects inst ->
+        (* 对于有副作用的指令，清空表达式映射 *)
+        Hashtbl.clear expr_map;
+        inst
+    | _ -> inst
+  in
+  
+  (* 处理所有指令 *)
+  let new_insts = List.map process_inst block.insts in
   { block with insts = new_insts }
 
 (* 对所有基本块应用CSE *)
