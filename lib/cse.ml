1774ee6891978e6b9003ef19f518b2fff7a9e869
辛屹 <163866348+xinkyu@users.noreply.github.com>
2025-08-10 12:52:56 +0800

    5.0.10

--- lib/cse.ml
+++ lib/cse.ml
@@ -12,146 +12,103 @@ let make_signature = function
   | Unop (op, _, src) -> Some (UnopSig (op, src))
   | _ -> None
 
-(* 检查指令是否有副作用 *)
-let has_side_effects = function
-  | Call _ | Load _ | Store _ -> true
+(* 表达式赋值记录 *)
+type expr_entry = {
+  result: operand;
+  orig_expr: ir_inst;
+}
+
+(* 检查表达式是否可能被修改 *)
+let invalidates expr inst =
+  let invalidated_by_write dst =
+    match expr, dst with
+    | BinopSig(_, Reg r1, _), Reg r2 
+    | BinopSig(_, _, Reg r1), Reg r2
+    | BinopSig(_, Var r1, _), Var r2
+    | BinopSig(_, _, Var r1), Var r2
+    | UnopSig(_, Reg r1), Reg r2
+    | UnopSig(_, Var r1), Var r2 -> r1 = r2
+    | _ -> false
+  in
+  match inst with
+  | Binop(_, dst, _, _)
+  | Unop(_, dst, _)
+  | Assign(dst, _)
+  | Call(dst, _, _)
+  | Load(dst, _) -> invalidated_by_write dst
+  | Store(_, _) -> true  (* 保守处理，任何内存写操作都可能影响其他值 *)
+  | Call(_, _, _) -> true  (* 函数调用可能有副作用 *)
   | _ -> false
 
-(* 从操作数获取名称 *)
-let get_reg_name = function
-  | Reg r | Var r -> r
-  | _ -> ""
-
-(* 比较两个操作数是否是相同的寄存器/变量 *)
-let same_reg op1 op2 =
-  match (op1, op2) with
-  | (Reg r1 | Var r1), (Reg r2 | Var r2) -> r1 = r2
+(* 检查操作数是否是临时寄存器 *)
+let is_temp = function
+  | Reg r -> String.length r > 0 && r.[0] = 't'
   | _ -> false
 
-(* 检查操作数是否是立即数 *)
-let is_imm = function
-  | Imm _ -> true
-  | _ -> false
-
-(* 提取操作数中的所有寄存器/变量 *)
-let extract_regs op =
-  match op with
-  | Reg r | Var r -> [r]
-  | _ -> []
-
-(* 安全的CSE实现 - 更加保守的方法 *)
+(* 更高效的CSE实现 *)
 let apply_cse (block : ir_block) : ir_block =
-  (* 收集已经定义的寄存器名称 *)
-  let defined_regs = Hashtbl.create 50 in
-  
-  (* 为了安全起见，先收集所有被定义的寄存器 *)
-  List.iter (fun inst ->
-    match inst with
-    | Binop (_, dst, _, _) 
-    | Unop (_, dst, _)
-    | Assign (dst, _)
-    | Call (dst, _, _)
-    | Load (dst, _) ->
-        (match dst with
-        | Reg r | Var r -> Hashtbl.replace defined_regs r ()
-        | _ -> ())
-    | _ -> ()
-  ) block.insts;
-  
-  (* 表达式到寄存器的映射 *)
-  let expr_map = Hashtbl.create 50 in
-  
-  (* 更加安全的CSE - 跟踪活跃的寄存器 *)
-  let live_regs = Hashtbl.create 50 in
-  List.iter (fun r -> Hashtbl.add live_regs r ()) 
-    (List.fold_left (fun acc inst ->
-      match inst with
-      | Binop (_, _, lhs, rhs) -> 
-          acc @ (extract_regs lhs) @ (extract_regs rhs)
-      | Unop (_, _, src) -> 
-          acc @ (extract_regs src)
-      | Assign (_, src) ->
-          acc @ (extract_regs src)
-      | _ -> acc
-    ) [] block.insts);
+  (* 表达式 -> 结果寄存器 映射 *)
+  let expr_table = Hashtbl.create 50 in
   
   (* 处理单个指令 *)
-  let process_inst inst =
-    match inst with
-    | Binop (_, dst, lhs, rhs) as binop ->
-        (* 只考虑两个操作数都是已定义寄存器或立即数的情况 *)
-        (match (lhs, rhs) with
-        | (Reg l | Var l), (Reg r | Var r) 
-            when Hashtbl.mem live_regs l && Hashtbl.mem live_regs r ->
-            let sig_opt = make_signature binop in
-            (match sig_opt with
-            | Some signature ->
-                (match Hashtbl.find_opt expr_map signature with
-                | Some existing_reg when Hashtbl.mem live_regs (get_reg_name existing_reg) ->
-                    (* 使用已有表达式替代 *)
-                    Assign (dst, existing_reg)
-                | _ ->
-                    (* 记录新表达式 *)
-                    (match dst with
-                    | Reg d | Var d -> 
-                        Hashtbl.replace live_regs d ();
-                        (match sig_opt with
-                        | Some s -> Hashtbl.replace expr_map s dst
-                        | None -> ())
-                    | _ -> ());
-                    inst)
-            | None -> inst)
-        | _ -> inst)
+  let rec process_insts acc insts =
+    match insts with
+    | [] -> List.rev acc
+    
+    | (Binop(op, dst, lhs, rhs) as expr) :: rest ->
+        let sig_opt = Some (BinopSig(op, lhs, rhs)) in
+        (match sig_opt with
+        | Some signature ->
+            (match Hashtbl.find_opt expr_table signature with
+            | Some entry ->
+                (* 找到了相同的表达式，用已有结果替代 *)
+                let new_inst = Assign(dst, entry.result) in
+                process_insts (new_inst :: acc) rest
+            | None ->
+                (* 记录新表达式 *)
+                Hashtbl.add expr_table signature {result = dst; orig_expr = expr};
+                process_insts (expr :: acc) rest)
+        | None -> process_insts (expr :: acc) rest)
+    
+    | (Unop(op, dst, src) as expr) :: rest ->
+        let sig_opt = Some (UnopSig(op, src)) in
+        (match sig_opt with
+        | Some signature ->
+            (match Hashtbl.find_opt expr_table signature with
+            | Some entry ->
+                (* 找到了相同的表达式，用已有结果替代 *)
+                let new_inst = Assign(dst, entry.result) in
+                process_insts (new_inst :: acc) rest
+            | None ->
+                (* 记录新表达式 *)
+                Hashtbl.add expr_table signature {result = dst; orig_expr = expr};
+                process_insts (expr :: acc) rest)
+        | None -> process_insts (expr :: acc) rest)
+    
+    | (Call _ | Store _) :: rest ->
+        (* 对于有副作用的指令，清空表达式表 *)
+        Hashtbl.clear expr_table;
+        process_insts (List.hd insts :: acc) rest
         
-    | Unop (_, dst, src) as unop ->
-        (* 只考虑操作数是已定义寄存器的情况 *)
-        (match src with
-        | Reg s | Var s when Hashtbl.mem live_regs s ->
-            let sig_opt = make_signature unop in
-            (match sig_opt with
-            | Some signature ->
-                (match Hashtbl.find_opt expr_map signature with
-                | Some existing_reg when Hashtbl.mem live_regs (get_reg_name existing_reg) ->
-                    (* 使用已有表达式替代 *)
-                    Assign (dst, existing_reg)
-                | _ ->
-                    (* 记录新表达式 *)
-                    (match dst with
-                    | Reg d | Var d -> 
-                        Hashtbl.replace live_regs d ();
-                        (match sig_opt with
-                        | Some s -> Hashtbl.replace expr_map s dst
-                        | None -> ())
-                    | _ -> ());
-                    inst)
-            | None -> inst)
-        | _ -> inst)
-        
-    | Assign (dst, _) ->
-        (* 更新活跃寄存器表 *)
-        (match dst with
-        | Reg d | Var d -> Hashtbl.replace live_regs d ()
-        | _ -> ());
-        inst
-        
-    | Call _ ->
-        (* 对于函数调用，清空表达式映射（保守处理） *)
-        Hashtbl.clear expr_map;
-        (match inst with
-        | Call (dst, _, _) ->
-            (match dst with
-            | Reg d | Var d -> Hashtbl.replace live_regs d ()
-            | _ -> ())
-        | _ -> ());
-        inst
-        
-    | _ -> inst
+    | inst :: rest ->
+        (* 检查指令是否使表达式表中的任何条目失效 *)
+        Hashtbl.filter_map_inplace (fun key entry ->
+            if invalidates key inst then None else Some entry
+        ) expr_table;
+        process_insts (inst :: acc) rest
   in
   
   (* 处理所有指令 *)
-  let new_insts = List.map process_inst block.insts in
+  let new_insts = process_insts [] block.insts in
   { block with insts = new_insts }
 
-(* 对所有基本块应用CSE *)
+(* 对所有基本块应用CSE，并多次应用以捕获更多优化机会 *)
 let common_subexpr_elimination (blocks : ir_block list) : ir_block list =
-  List.map apply_cse blocks
+  (* 多次应用CSE以获得更好的结果 *)
+  let rec optimize_blocks blocks iterations =
+    if iterations = 0 then blocks
+    else
+      let optimized = List.map apply_cse blocks in
+      optimize_blocks optimized (iterations - 1)
+  in
+  optimize_blocks blocks 2  (* 应用两轮CSE *)
